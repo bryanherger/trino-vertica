7ad9661c7d6081a86d3e4d322264602d3068415c
bryanherger <bryanherger@gmail.com>
2023-06-18 13:40:07 -0400

    added aggregate tests

--- src/test/java/io/trino/plugin/vertica/TestVerticaQueries.java
+++ src/test/java/io/trino/plugin/vertica/TestVerticaQueries.java
@@ -32,21 +32,38 @@ public class TestVerticaQueries
     public void showTables()
     {
         assertQuery("SHOW SCHEMAS FROM vertica LIKE 't%'", "VALUES 'trino'");
-        assertQuery("SHOW TABLES FROM vertica.trino", "VALUES 'test'");
+        assertQuery("SHOW TABLES FROM vertica.trino LIKE 'test%'", "VALUES 'test'");
     }
 
     @Test
     public void createAndDropTable()
     {
         assertQuerySucceeds("create table vertica.trino.test2 (a int, b date, c varchar)");
-        assertQuery("SHOW TABLES FROM vertica.trino", "VALUES 'test','test2'");
+        assertQuery("SHOW TABLES FROM vertica.trino LIKE 'test%'", "VALUES 'test','test2'");
         assertQuerySucceeds("drop table vertica.trino.test2");
-        assertQuery("SHOW TABLES FROM vertica.trino", "VALUES 'test'");
+        assertQuery("SHOW TABLES FROM vertica.trino LIKE 'test%'", "VALUES 'test'");
     }
 
     @Test
     public void selectFromTable()
     {
         assertQuery("SELECT DISTINCT v FROM vertica.trino.test", "VALUES 'Vertica', 'Trino'");
+    }
+
+    @Test
+    public void aggregates()
+    {
+        assertQuerySucceeds("create table vertica.trino.unitAgg (a int, b double)");
+        assertQuerySucceeds("INSERT INTO vertica.trino.unitAgg values (1,1.23)");
+        assertQuerySucceeds("INSERT INTO vertica.trino.unitAgg values (2,2.23)");
+        assertQuerySucceeds("INSERT INTO vertica.trino.unitAgg values (3,3.23)");
+        assertQuery("SELECT SUM(a) FROM vertica.trino.unitAgg", "VALUES 6");
+        assertQuery("SELECT AVG(a) FROM vertica.trino.unitAgg", "VALUES 2");
+        assertQuery("SELECT SUM(b) FROM vertica.trino.unitAgg", "VALUES 6.69");
+        assertQuery("SELECT AVG(b) FROM vertica.trino.unitAgg", "VALUES 2.23");
+        assertQuery("SELECT COUNT(*) FROM vertica.trino.unitAgg", "VALUES 3");
+        assertQuery("SELECT MIN(a) FROM vertica.trino.unitAgg", "VALUES 1");
+        assertQuery("SELECT MAX(a) FROM vertica.trino.unitAgg", "VALUES 3");
+        assertQuerySucceeds("drop table vertica.trino.unitAgg");
     }
 }
